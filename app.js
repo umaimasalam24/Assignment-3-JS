d1fc1e30e04c8ba319bc23ab1a9f3c5e4ba3efca
Umaima Salam <umaimasalam2003@gmail.com>
2025-06-19 15:26:41 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+// 1. Declare a variable called age & assign to it your age. Show your age in an alert box.
+// let age = prompt("Enter Your Age");
+// alert("I am " + age + " years old.")
+
+
+// 2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+// let x = ("You have visited this site 15 times");
+// alert(x);
+
+
+// 3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser
+// let birthYear = prompt("Enter Your Birth Year");
+// document.writeln("My Birth year is " + birthYear , "<br>" , "Data type of my Declared variable is number.");
+
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+// let VisitorName = prompt("Enter Your Name:");
+// let Order = prompt("What do you want to order?");
+// let Quantity = prompt("How many products you want?");
+// document.writeln(VisitorName  + " ordered " + Quantity + Order + " from my Clothing store. " )
